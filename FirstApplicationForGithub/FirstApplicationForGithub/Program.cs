ad938d21a644e902e98f0b306a86464f86f536f8
Abdul Khursheed <abdulkhursheed123@gmail.com>
2020-03-27 00:09:39 +0530

    made changes in config

--- FirstApplicationForGithub/FirstApplicationForGithub/Program.cs
+++ FirstApplicationForGithub/FirstApplicationForGithub/Program.cs
@@ -21,6 +21,7 @@ namespace FirstApplicationForGithub
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    //Hello Abdul
                 });
     }
 }

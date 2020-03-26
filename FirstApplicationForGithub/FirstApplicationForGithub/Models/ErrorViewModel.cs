1e6f4e2f514143f9d2cea120a8eb214617e89208
Abdul Khursheed <abdulkhursheed123@gmail.com>
2020-03-27 00:04:08 +0530

    Adding project

---
+++ FirstApplicationForGithub/FirstApplicationForGithub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstApplicationForGithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
